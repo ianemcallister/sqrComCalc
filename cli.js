66ec0c7765d9cad1cac46d52c2c8e364e7c7e6f7
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2019-10-17 07:46:35 -0700

    Add: CLI

---
+++ cli.js
@@ -0,0 +1,13 @@
+/*
+*	CLI
+*
+*	This is the command line interface script
+*/
+
+//  DECLARE DEPENDENCIES
+
+//	DEFINE LOCAL VARIABLES
+let dateparam = process.argv[2]
+
+//	NOTIFY PROGRESS
+console.log(dateparam);
