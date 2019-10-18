3a12e320ea4713b4e58c0c7da5c9b0d507f85aa5
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2019-10-18 07:54:25 -0700

    Add: comCalculator Module

--- cli.js
+++ cli.js
@@ -5,9 +5,11 @@
 */
 
 //  DECLARE DEPENDENCIES
+let comCalculator 	= require('./comCalculator.js');
 
 //	DEFINE LOCAL VARIABLES
 let dateparam = process.argv[2]
 
 //	NOTIFY PROGRESS
 console.log(dateparam);
+comCalculator.test();
---
+++ comCalculator.js
@@ -0,0 +1,20 @@
+/*
+*	COMMISSION CALCULATOR
+*
+*	This is the commission calculator module
+*/
+
+//  DECLARE DEPENDENCIES
+
+//	DEFINE THE MODULE
+let commissionCalculator = {
+    test: function() { console.log('good test'); }
+};
+
+//	DEFINE LOCAL VARIABLES
+
+//	NOTIFY PROGRESS
+
+
+//  EXPORT THE MODULE
+module.exports = commissionCalculator;
