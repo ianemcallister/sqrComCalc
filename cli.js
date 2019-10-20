8bba37b36f8cc30cf31752f9174bb63be0ea150b
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2019-10-20 09:20:20 -0700

    Add: Daily Commissions Function

--- cli.js
+++ cli.js
@@ -11,5 +11,23 @@ let comCalculator 	= require('./comCalculator.js');
 let dateparam = process.argv[2]
 
 //	NOTIFY PROGRESS
-console.log(dateparam);
-comCalculator.test();
+
+//	RUN
+if(dateparam != undefined) {
+
+	//	RUN ASYNC WORK - only if a valid dateparam is present
+	comCalculator.dailyCommissions(dateparam)
+	.then(function success(commissionsReport) {
+
+		//	NOTIFY RESULTS
+		console.log(commissionsReport);
+
+	}).catch(function error(e) {
+	    console.error('There was an error:', e);
+	});
+	
+} else {
+	//	NOTIFY ERROR - if no valid dateparam is present
+	console.error('INVALID DATA PARAMETER, PLEASE ENTER VALID DATE PARAMETER "YYYY-MM-DD"')
+}
+
--- comCalculator.js
+++ comCalculator.js
@@ -10,6 +10,7 @@ let firebase	= require('./firebaseAPI.js');
 
 //	DEFINE THE MODULE
 let commissionCalculator = {
+	dailyCommissions: dailyCommissions, 
     test: function() { console.log('good comCalc test'); }
 };
 
@@ -17,6 +18,29 @@ let commissionCalculator = {
 
 //	NOTIFY PROGRESS
 
+//  PUBLIC FUNCTIONS
+/*
+*	DAILY COMMISIONS
+*
+*	This function 
+*	@param {string} date to be searched YYYY-MM-DD
+*	@return {Object} collection of employees and their commissions
+*/
+function dailyCommissions(date) {
+	//    DEFINE LOCAL VARIABLES
+	let allDataPromises = [
+		square.getDailyTransactions(date),
+		firebase.getAllEmployees()
+	];
+
+	//    WAIT FOR ALL PROMISES TO RESOLVE BEFORE PROCESSSING DATA
+    Promise.all(allDataPromises)
+    .then(function success(allDataArray) {
+    	resolve(allDataArray);
+    }).catch(function error(e) {
+    	reject(e);
+    });
+};
 
 //  EXPORT THE MODULE
 module.exports = commissionCalculator;
